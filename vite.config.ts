185973b0f151fe03260fbfbf9c750c4185cf782a
Alex Avila <alex.ramon2004@gmail.com>
2025-12-03 20:07:59 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/eco-guardian-frontend/",
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
